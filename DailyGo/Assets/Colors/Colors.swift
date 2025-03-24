f1d61ea657a6e6420de449d72a8c60b032f0b2e4
Danila Petrov <danilapetrov@Noutbuk-Danila.local>
2025-03-24 17:00:49 +0500

    DG-1: Add colors

---
+++ DailyGo/Assets/Colors/Colors.swift
@@ -0,0 +1,42 @@
+//
+//  Colors.swift
+//  DailyGo
+//
+//  Created by Danila Petrov on 24.03.2025.
+//
+
+import UIKit
+
+public enum Colors {
+    public static let aquaMist = UIColor(resource: .aquaMist)
+    public static let ashGray = UIColor(resource: .ashGray)
+    public static let babyBlue = UIColor(resource: .babyBlue)
+    public static let blushPink = UIColor(resource: .blushPink)
+    public static let brightBlue = UIColor(resource: .brightBlue)
+    public static let bubblegum = UIColor(resource: .bubblegum)
+    public static let charcoal = UIColor(resource: .charcoal)
+    public static let chestnutBrown = UIColor(resource: .chestnutBrown)
+    public static let coralPink = UIColor(resource: .coralPink)
+    public static let crimsonRed = UIColor(resource: .crimsonRed)
+    public static let deepOcean = UIColor(resource: .deepOcean)
+    public static let deepTeal = UIColor(resource: .deepTeal)
+    public static let goldenrod = UIColor(resource: .goldenrod)
+    public static let hotPink = UIColor(resource: .hotPink)
+    public static let icyBlue = UIColor(resource: .icyBlue)
+    public static let lavender = UIColor(resource: .lavender)
+    public static let lavenderBlue = UIColor(resource: .lavenderBlue)
+    public static let lightSky = UIColor(resource: .lightSky)
+    public static let mustardYellow = UIColor(resource: .mustardYellow)
+    public static let oliveGreen = UIColor(resource: .oliveGreen)
+    public static let pastelPeach = UIColor(resource: .pastelPeach)
+    public static let peach = UIColor(resource: .peach)
+    public static let periwinkle = UIColor(resource: .periwinkle)
+    public static let raspberry = UIColor(resource: .raspberry)
+    public static let sageGreen = UIColor(resource: .sageGreen)
+    public static let sandstone = UIColor(resource: .sandstone)
+    public static let skyBlue = UIColor(resource: .skyBlue)
+    public static let softCyan = UIColor(resource: .softCyan)
+    public static let softGray = UIColor(resource: .softGray)
+    public static let turquoise = UIColor(resource: .turquoise)
+    public static let vividOrange = UIColor(resource: .vividOrange)
+}
