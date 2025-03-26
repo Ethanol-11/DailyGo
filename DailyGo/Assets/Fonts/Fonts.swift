881f18d40eef0e0b608681f90f48a55d6c5206e6
Danila Petrov <danilapetrov@Noutbuk-Danila.local>
2025-03-26 17:26:31 +0500

    DG-3: Add fonts

---
+++ DailyGo/Assets/Fonts/Fonts.swift
@@ -0,0 +1,24 @@
+//
+//  Fonts.swift
+//  DailyGo
+//
+//  Created by Danila Petrov on 26.03.2025.
+//
+
+import UIKit
+
+public enum Fonts {
+    public static let anonymousPro_10 = UIFont(name: "AnonymousPro-Regular", size: 10.0)
+    public static let anonymousPro_12 = UIFont(name: "AnonymousPro-Regular", size: 12.0)
+    public static let anonymousPro_13 = UIFont(name: "AnonymousPro-Regular", size: 13.0)
+    public static let anonymousPro_14 = UIFont(name: "AnonymousPro-Regular", size: 14.0)
+    public static let anonymousPro_15 = UIFont(name: "AnonymousPro-Regular", size: 15.0)
+    public static let anonymousPro_16 = UIFont(name: "AnonymousPro-Regular", size: 16.0)
+    public static let anonymousPro_20 = UIFont(name: "AnonymousPro-Regular", size: 20.0)
+    public static let anonymousPro_24 = UIFont(name: "AnonymousPro-Regular", size: 24.0)
+
+    public static let peachCream_10 = UIFont(name: "Peach Cream", size: 10.0)
+    public static let peachCream_20 = UIFont(name: "Peach Cream", size: 20.0)
+    public static let peachCream_24 = UIFont(name: "Peach Cream", size: 24.0)
+    public static let peachCream_48 = UIFont(name: "Peach Cream", size: 48.0)
+}
